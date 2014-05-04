18c36fbb9d364ebcc8181146c402987cdbb8faa8
Brandon Mathis <brandon@imathis.com>
2014-05-04 17:20:35 -0500

    Fixed improper require path for local_asset_pipleline plugin

--- lib/octopress-ink.rb
+++ lib/octopress-ink.rb
@@ -190,7 +190,7 @@ Liquid::Template.register_tag('_', Octopress::Ink::Tags::LineCommentTag)
 Liquid::Template.register_tag('doc_url', Octopress::Ink::Tags::DocUrlTag)
 
 require 'octopress-ink/plugins/ink'
-require 'octopress-ink/plugins/asset_pipeline'
+require 'octopress-ink/plugins/local_asset_pipeline'
 
 Octopress::Ink.register_plugin(Octopress::Ink::InkPlugin)
 Octopress::Ink.register_plugin(Octopress::Ink::AssetPipelinePlugin)
