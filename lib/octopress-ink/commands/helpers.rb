d2bf77cf2ec1dd67fd6868fd3de63559ee6f7861
Brandon Mathis <imathis@blip.local>
2015-04-28 23:25:42 -0500

    Improved bundler integration for creating new plugins, simplified flags for list and copy sub commands

--- lib/octopress-ink/commands/helpers.rb
+++ lib/octopress-ink/commands/helpers.rb
@@ -3,21 +3,20 @@ module Octopress
     module Commands
       module CommandHelpers
         def self.add_asset_options(c, action)
-          c.option "config-file", "--config-file", "#{action} plugin's default configuration"
-          c.option "layouts", "--layouts", "#{action} only layouts"
-          c.option "includes", "--includes", "#{action} only includes"
-          c.option "pages", "--pages", "#{action} only pages"
-          c.option "templates", "--templates", "#{action} only pages"
-          c.option "stylesheets", "--stylesheets", "#{action} only Stylesheets (.css, .scss, .sass)"
+          c.option "config-file", "-c", "--config", "#{action} plugin's config file"
+          c.option "layouts", "-l", "--layouts", "#{action} only layouts"
+          c.option "includes", "-i", "--includes", "#{action} only includes"
+          c.option "pages", "-p", "--pages", "#{action} only pages"
+          c.option "templates", "-t", "--templates", "#{action} only pages"
+          c.option "stylesheets", "-s", "--stylesheets", "#{action} only Stylesheets (.css, .scss, .sass)"
           c.option "css", "--css", "#{action} only CSS files (.css)"
           c.option "sass", "--sass", "#{action} only Sass files (.scss, .sass)"
-          c.option "javascripts", "--javascripts", "#{action} only Javascripts (.js and .coffee)"
+          c.option "javascripts", "-j", "--javascripts", "#{action} only Javascripts (.js and .coffee)"
           c.option "js", "--js", "#{action} only Javascript files (.js)"
           c.option "coffee", "--coffee", "#{action} only Coffeescript files (.coffee)"
-          c.option "images", "--images", "#{action} only images"
+          c.option "images", "-img", "--images", "#{action} only images"
           c.option "fonts", "--fonts", "#{action} only fonts"
           c.option "files", "--files", "#{action} only files"
-          c.option "config", "--config", "#{action} only configuration file."
         end
       end
     end
--- test/standard/_expected/test-theme/test-theme.gemspec
+++ test/standard/_expected/test-theme/test-theme.gemspec
@@ -8,16 +8,25 @@ Gem::Specification.new do |spec|
   spec.version       = TestTheme::VERSION
   spec.authors       = ["TODO: Write your name"]
   spec.email         = ["TODO: Write your email address"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
-  spec.homepage      = ""
-  spec.license       = "MIT"
+
+  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
+  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+
+  # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
+  # delete this section to allow pushing this gem to any host.
+  if spec.respond_to?(:metadata)
+    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
+  else
+    raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
+  end
 
   spec.files         = `git ls-files -z`.split("\x0").grep(%r{^(bin/|lib/|assets/|changelog|readme|license)}i)
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.bindir        = "exe"
+  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.7"
+  spec.add_development_dependency "bundler", "~> 1.9"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "clash"
   
