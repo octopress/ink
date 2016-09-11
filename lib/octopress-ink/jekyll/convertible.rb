a81b195c11824f1539915138a99047df1b5b1282
Randy Coulman <rcoulman@gmail.com>
2016-09-10 22:26:53 -0700

    Fix undefined method merged_file_read_opts
    The `merged_file_read_opts` method is in the `Jekyll::Utils` module and
    takes a `site` argument.
    
    Note that this is the same fix as in #65; I just wanted to have a
    functional version of ink for Jekyll 3.

--- lib/octopress-ink/jekyll/convertible.rb
+++ lib/octopress-ink/jekyll/convertible.rb
@@ -2,6 +2,7 @@ module Octopress
   module Ink
     module Convertible
       include Jekyll::Convertible
+      include Jekyll::Utils
 
       # Read the YAML frontmatter.
       #
@@ -12,7 +13,7 @@ module Octopress
       # Returns nothing.
       def read_yaml(base, name, opts = {})
         begin
-          self.content = File.read(File.join(base, name), merged_file_read_opts(opts))
+          self.content = File.read(File.join(base, name), merged_file_read_opts(site, opts))
           if content =~ /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m
             self.content = $POSTMATCH
             self.data = SafeYAML.load($1)
